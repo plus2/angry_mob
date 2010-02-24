2f3cde140b3514ec7942e19dddad441db351bc40
lachie <lachiec@gmail.com>
2010-02-24 18:23:08 +1100

    vendor hashie

--- lib/angry_mob.rb
+++ lib/angry_mob.rb
@@ -1,5 +1,12 @@
 require 'extend'
 
+here = Pathname.here(__FILE__)
+
+# vendorage... what's the best way?
+$LOAD_PATH << here+'vendor/hashie-0.1.8/lib'
+
+require 'hashie'
+
 class AngryMob
   autoload :Mob    , 'angry_mob/mob'
   autoload :Act    , 'angry_mob/act'
