db443a0299a0532f9d8e45859d3a61c96be5ff05
lachie <lachiec@gmail.com>
2010-02-24 18:28:13 +1100

    lots of work for binding nodes

--- lib/angry_mob/node.rb
+++ lib/angry_mob/node.rb
@@ -1,14 +1,55 @@
+
 class AngryMob
   class Node < Struct.new(:name,:attributes)
+    def initialize(name, attributes)
+      self.name = name
+      self.attributes = Hashie::Mash.new(attributes)
+    end
+
     def targets
       @targets ||= []
     end
 
-    def method_missing(method,*args,&block)
-      puts "Node mm #{method}"
-      # TODO - autovivification, tree-like, or Hashie::Mash
+    def delayed_targets
+      @delayed_targets ||= []
+    end
 
-      # gulp
+    def act_names
+      @act_names ||= attributes[:acts] || []
+    end
+
+    def start_iterating!
+      unless @iterating
+        @iterating_act_names = act_names.dup
+        @iterating = true
+      end
+    end
+
+    def next_act
+      start_iterating!
+      @iterating_act_names.pop
+    end
+
+    def notify(notification)
+      later = [ notification[:later] ].flatten.compact
+      now   = [ notification[:now  ] ].flatten.compact
+
+      now.each {|n| n.call(node)}
+
+      delayed_targets += later
+    end
+
+    def merge_defaults(attrs)
+      attributes.replace( Hashie::Mash.new(attrs).update(attributes) )
+    end
+
+    def schedule_act(*acts)
+      acts.tapp
+      # TODO implement
+    end
+
+    def method_missing(method,*args,&block)
+      attributes.send(method,*args)
     end
   end
 end
