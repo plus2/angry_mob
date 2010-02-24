8e401e4642862e9599ba06d8c712534985ce0c06
lachie <lachiec@gmail.com>
2010-02-24 18:24:28 +1100

    builder provides a nice dsl for binding various definitions (taken from files) to mobs

--- lib/angry_mob/builder.rb
+++ lib/angry_mob/builder.rb
@@ -1,4 +1,135 @@
+require 'pathname'
 class AngryMob
+  class TargetCreationContext
+    def initialize(&blk)
+      @blk = blk
+    end
+
+    def bind(mob)
+      @mob = mob
+      instance_eval(&@blk)
+      self
+    end
+
+    def SingletonTarget(nickname,&blk)
+      @mob.target_classes[nickname] = Class.new(SingletonTarget, &blk)
+    end
+
+    def Target(nickname,&blk)
+      @mob.target_classes[nickname] = Class.new(Target, &blk)
+    end
+    
+    def method_missing(method,*args,&blk)
+      if args.size == 1
+        @mob.target_classes[args.first] = Class.new(@mob.target_classes[method], &blk)
+      else
+        super
+      end
+    end
+  end
+
+  class BuilderDefaults
+    def initialize
+      @contexts = Hash.new {|h,k| h[k] = []}
+    end
+
+    def defaults_for(name)
+      # TODO - merge stack
+      @contexts[name].last || {}
+    end
+
+    def method_missing(method, *args, &blk)
+      @contexts[method] << args.first
+      yield
+      @contexts[method].pop
+    end
+  end
+
+  class ActCreationContext
+    def initialize(name,&blk)
+      @name = name
+      @blk = blk
+    end
+
+    def bind(mob)
+      @mob = mob
+      mob.acts[@name] = self
+    end
+
+    def compile!(node)
+      @node = node
+      @act = Act.new(@name)
+      instance_exec node, &@blk
+    end
+
+    def defaults
+      @defaults ||= BuilderDefaults.new
+    end
+
+    def method_missing(method,*args,&blk)
+      @node.targets << t = @mob.target(method,*args,&blk)
+
+      t.merge_defaults(defaults.defaults_for(method)) if t.respond_to?(:merge_defaults)
+
+      t
+    end
+  end
+
+
   class Builder
+    def self.from_file(path)
+      path = Pathname(path)
+      new.from_file(path)
+    end
+
+    def from_file(path)
+      instance_eval path.read, path.to_s
+      self
+    end
+
+    def initialize
+    end
+
+    def node_setup_blocks
+      @node_setup_blocks ||= []
+    end
+
+    def setup_node(&blk)
+      node_setup_blocks << blk
+    end
+
+    def acts
+      @acts ||= Dictionary.new
+    end
+
+    def target_blocks
+      @target_blocks ||= []
+    end
+
+    def act(name, &blk)
+      acts[name] = blk
+    end
+
+    def targets(&blk)
+      target_blocks << blk
+    end
+
+    def to_mob
+      mob = Mob.new
+
+      mob.setup_node = lambda {|node|
+        node_setup_blocks.each {|blk| blk[node]}
+      }
+
+      target_blocks.each do |blk|
+        TargetCreationContext.new(&blk).bind(mob)
+      end
+
+      acts.each do |name,blk|
+        ActCreationContext.new(name,&blk).bind(mob)
+      end
+
+      mob
+    end
   end
 end
