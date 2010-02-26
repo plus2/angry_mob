d89a71c96cd42509a6a1128c20ea6ddfd580dd97
lachie <lachiec@gmail.com>
2010-02-26 23:31:54 +1100

    debug, too

--- lib/angry_mob/log.rb
+++ lib/angry_mob/log.rb
@@ -1,7 +1,12 @@
 class AngryMob
 	module Log
+
 		def log(*msg)
-			puts "%-20s| #{msg * ' '}" % self.class.to_s
+			puts "  %-20s| #{msg * ' '}" % self.class.to_s
+		end
+
+		def debug(*msg)
+			puts "* %-20s| #{msg * ' '}" % self.class.to_s
 		end
 	end
 end
