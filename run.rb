f972a149bf140ff906d8eec087489f116dd95895
lachie <lachiec@gmail.com>
2010-03-03 08:43:18 +1100

    testing - rsync & run on linode slice

---
+++ run.rb
@@ -0,0 +1,24 @@
+Server = "lstaging"
+
+def sh(cmd)
+  puts "sh: #{cmd}"
+  system cmd
+  raise "failed: #{cmd}" unless $?.success?
+end
+
+def rsync(from,to)
+  sh "rsync -avz --exclude=.git #{from}/ #{Server}:#{to}/"
+end
+
+dos = { :am => true, :p2m => true, :dna => true  }
+dos = { :am => true, :p2m => true, :dna => false }
+
+dos[:am] && rsync('.'          , 'angry_mob')
+dos[:p2m] && rsync('../plus2mob', 'plus2mob')
+
+dos[:dna] && Dir.chdir("../server_dna") do
+  sh("bundle exec rake json")
+  sh("rsync -avz server_dna.json #{Server}:server_dna.json")
+end
+
+system "ssh #{Server} angry_mob/bin/mob"
