f8a462c75de7968486b0f29d7caea1f7ce5cd33e
lachie <lachiec@gmail.com>
2010-03-03 20:05:29 +1100

    Array#options read or create options as last arg

--- lib/extend/array.rb
+++ lib/extend/array.rb
@@ -3,6 +3,16 @@ class Array
 		if Hash === last then pop else {} end
 	end
 	def extract_options
-		dup.extract_options!
+		if Hash === last then last else {} end
 	end
+
+  def options
+    if Hash === last
+      self[-1] = AngryMob::AngryHash.__convert(last)
+    else
+      opts = AngryMob::AngryHash.new
+      push opts
+      opts
+    end
+  end
 end
