29df4dd58d9ebf26671f20b8cc2c8685ef4796c5
lachie <lachiec@gmail.com>
2010-03-05 11:36:38 +1100

    node config in a case statement

--- run.rb
+++ run.rb
@@ -1,5 +1,35 @@
-# Server = "lstaging"
-Server = "frog"
+@home = '/root'
+@sudo = false
+
+server = 'frog-clean'
+@dry_run = false
+
+case server
+when 'lstaging'
+  @server   = "lstaging"
+  @nodename = "plus2staging-new"
+
+when 'bho'
+  @server = 'bho'
+  @nodename = 'bho'
+
+when 'amc-store'
+  @home = '/home/vpsadmin'
+  @server   = "amcstore"
+  @nodename = "amc-store"
+  @sudo = true
+
+when 'amc-exam'
+  @home = '/home/vpsadmin'
+  @server   = "amcexam"
+  @nodename = "amc-exam"
+  @sudo = true
+
+when 'frog-clean'
+  @server = "frog"
+  @server = "root@192.168.0.12"
+  @nodename = "frog"
+end
 
 def sh(cmd)
   puts "sh: #{cmd}"
@@ -8,18 +38,26 @@ def sh(cmd)
 end
 
 def rsync(from,to)
-  sh "rsync -avz --exclude=.git --delete #{from}/ #{Server}:#{to}/"
+  sh "rsync -avz --exclude=.git --exclude='.*.sw?' --delete #{from}/ #{@server}:#{to}/"
 end
 
 dos = { :am => true, :p2m => true, :dna => true  }
-dos = { :am => true, :p2m => true, :dna => true }
+dos = { :am => true, :p2m => true, :cm => true, :dna => true }
 
-dos[:am] && rsync('.'          , 'angry_mob')
-dos[:p2m] && rsync('../plus2mob', 'plus2mob')
+dos[:am]  && rsync('.'            , 'angry_mob' )
+dos[:p2m] && rsync('../plus2mob'  , 'plus2mob'  )
+dos[:cm]  && rsync('../common_mob', 'common_mob')
 
 dos[:dna] && Dir.chdir("../server_dna") do
   sh("bundle exec rake json")
-  sh("rsync -avz server_dna.json #{Server}:server_dna.json")
+  sh("rsync -avz server_dna.json #{@server}:server_dna.json")
 end
 
-system "ssh #{Server} angry_mob/bin/mob"
+@nodename ||= @server
+
+cmd = "ssh #{@server} #{'sudo' if @sudo} angry_mob/bin/mob --nodename #{@nodename} " \
+      "--mob #{@home}/plus2mob --mob #{@home}/common_mob --act plus2/basics --json-file #{@home}/server_dna.json "\
+      "#{'--dry-run' if @dry_run}"
+
+puts "running with #{cmd}"
+system cmd
