03847bc160b4971f74204775294ea8a53fed11d0
lachie <lachiec@gmail.com>
2010-03-05 11:36:17 +1100

    stapp probe: print classname and truncated inspect

--- lib/extend/object.rb
+++ lib/extend/object.rb
@@ -7,6 +7,13 @@ class Object
     self
 	end
 
+	def stapp(tag=nil)
+    print "#{tag} " if tag
+		puts "c=#{self.class} i=#{self.inspect[0..100]}"
+    self
+	end
+
+
 	def returning(obj)
 		yield obj
 		obj
