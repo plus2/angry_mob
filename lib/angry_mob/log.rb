9891fa83fd09b3a112965863bdf0ac23d00a6347
lachie <lachiec@gmail.com>
2010-03-05 11:35:30 +1100

    abbreviate class names where possible

--- lib/angry_mob/log.rb
+++ lib/angry_mob/log.rb
@@ -1,12 +1,22 @@
 class AngryMob
 	module Log
 
+		def __class_to_s
+			@__class_to_s ||= __class_to_s!
+		end
+		def __class_to_s!
+			@__class_to_s = self.class.to_s.sub(/^AngryMob::/,'AM::').sub(/^Target\[/,'T[')
+			@__class_to_s[0..19]
+		end
+
 		def log(*msg)
-			puts "  %-20s| #{msg * ' '}" % self.class.to_s
+			puts "  %-20s| #{msg * ' '}" % __class_to_s
+		rescue
+			puts "#{self.class.to_s} | #{msg * ' '}"
 		end
 
 		def debug(*msg)
-			puts "* %-20s| #{msg * ' '}" % self.class.to_s
+			# puts "* %-20s| #{msg * ' '}" % __class_to_s
 		end
 	end
 end
