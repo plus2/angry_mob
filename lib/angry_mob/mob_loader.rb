fcc9baddbe299422b879fb77b113b3c1f6abc367
lachie <lachiec@gmail.com>
2010-05-05 09:25:39 +1000

    rejigging for new target approach

--- lib/angry_mob/mob_loader.rb
+++ lib/angry_mob/mob_loader.rb
@@ -1,16 +1,55 @@
+require 'tsort'
+
 class AngryMob
   class MobLoadingError < StandardError; end
-  class MobLoader
-    include Log
 
-    attr_reader :builder
+  # `TargetList` is a hash for topologically sorting target blocks. Thus it provides simple dependency handling.
+  class MobList < Hash
+    include TSort
+
+    alias_method :tsort_each_node, :each_key
+    def tsort_each_child(node, &block)
+      fetch(node)[:dependencies].each(&block)
+    end
+
+    def add_path(name,path,dependencies)
+      dependencies = [ dependencies ].flatten.compact.map {|k| k.to_s}
+      self[name.to_s] = {:path => blk, :dependencies => dependencies}
+    end
+
+    def each_target(&block)
+      each_strongly_connected_component do |name|
+        name = name.first
+        yield name, fetch(name)[:block]
+      end
+    end
+  end
+
+
+
+  class MobLoader
+    attr_reader :builder, :mobs, :loaded_mobs
 
     def initialize
       @builder = Builder.new
+      @mobs = Dictionary.new
+      @loaded_mobs = {}
     end
 
-    def load(path)
+    def add_mob(path,name=nil)
       path = Pathname(path).expand_path
+      name ||= path.basename.to_s
+
+      mobs[name] ||= path
+    end
+
+    def load_a_mob(name)
+      path = mobs[name]
+      puts "loading mob #{name} at path #{path}"
+      raise "unable to load unknown mob #{name}" unless path
+
+      return if loaded_mobs[name]
+      loaded_mobs[name] = true
 
       loader = path+'load.rb'
 
@@ -25,6 +64,21 @@ class AngryMob
       self
     end
 
+    def load_mobs
+      mobs.keys.each do |name|
+        load_a_mob(name)
+      end
+    end
+
+    def to_mob
+      load_mobs
+      @builder.to_mob
+    end
+
+
+    # API
+    alias_method :depends_on_mob, :load_a_mob
+
     def load_targets(path)
       # XXX
     end
@@ -34,15 +88,12 @@ class AngryMob
     end
 
     def load_mob(path)
-      log "loading mob from #{path}"
+      puts "loading mob from #{path}"
 
       Pathname.glob(path+'mob/**/*.rb').each do |file|
         @builder.from_file(file)
       end
     end
 
-    def to_mob
-      @builder.to_mob
-    end
   end
 end
